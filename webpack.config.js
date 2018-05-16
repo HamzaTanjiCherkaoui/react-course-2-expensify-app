66a83219af650058d4e2fa65f0234e0987178653
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2018-05-16 11:15:00 +0000

    tweaking webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = (env) => {
+    const isProduction = env === 'production';
 
-module.exports= {
-    entry:'./src/app.js',
-    output : {
-        path : path.join(__dirname,'public'),
-        filename: 'bundle.js'
-    },
-    module : {
-        rules : [{
-            loader : 'babel-loader',
-            test:/\.js$/,
-            exclude : /node_modules/
+    return {
+        entry:'./src/app.js',
+        output : {
+            path : path.join(__dirname,'public'),
+            filename: 'bundle.js'
         },
-        {
-            use :['style-loader','css-loader','sass-loader'],
-            test:/\.s?css$/
+        module : {
+            rules : [{
+                loader : 'babel-loader',
+                test:/\.js$/,
+                exclude : /node_modules/
+            },
+            {
+                use :['style-loader','css-loader','sass-loader'],
+                test:/\.s?css$/
+            }
+            ]
+        },
+        devtool : isProduction? 'source-map' : 'cheap-module-eval-source-map' ,
+        devServer : {
+            contentBase : path.join(__dirname,'public'),
+            historyApiFallback:true
         }
-        ]
-    },
-    devtool : 'cheap-module-eval-source-map',
-    devServer : {
-        contentBase : path.join(__dirname,'public'),
-        historyApiFallback:true
     }
-};
+}
 
