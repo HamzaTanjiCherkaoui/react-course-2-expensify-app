ce1442146690e9a9d08e2ebbddd5c4c91fcdd327
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2018-05-16 15:45:27 +0000

    formating the amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {Link } from 'react-router-dom';
-import moment from 'moment'
+import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 export const ExpenseListItem = ({id, description, amount, createdAt, dispatch}) => (
     <div>
-        <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
-        <p>{amount}
-            - {moment(createdAt).format('Do-MM-YYYY')}
+        <h3>
+            <Link to={`/edit/${id}`}>{description}</Link>
+        </h3>
+        <p>{numeral(amount/100).format('$0,0.00')}
+            - {moment(createdAt).format('MMMM Do, YYYY')}
         </p>
     </div>
 );
